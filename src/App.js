bdb1faee20e77ea62de8be4e4f133b7e089e655e
ColasCitron <ffnico@msn.com>
2023-09-20 11:28:38 +0200

    feat: :sparkles: added accordion menus to sidebar with href links

--- src/App.js
+++ src/App.js
@@ -1,40 +1,120 @@
+import React, { useState } from 'react';
+
+function Accordion1() {
+  const [isActive, setIsActive] = useState(false);
+  return(
+  <div>
+    <button onClick={() => setIsActive(!isActive)} class="collapsible">{isActive ? 'v' : '>'}</button><a href='#uml-structure'>Diagrammes de structure</a>
+    {isActive && <div>
+      <a href='#uml-class'>Classe</a> 
+      <a href='#uml-comp'>Composants</a> 
+      <a href='#uml-deploy'>Déploiement</a> 
+      <a href='#uml-objects'>Objets</a> 
+      <a href='#uml-package'>Paquetage</a> 
+      <a href='#uml-profile'>Profil</a> 
+      <a href='#uml-composite'>Structure composite</a>
+    </div>}
+  </div>
+  )
+}
+
+function Accordion2() {
+  const [isActive, setIsActive] = useState(false);
+  return(
+  <div>
+    <button onClick={() => setIsActive(!isActive)} class="collapsible">{isActive ? 'v' : '>'}</button><a href='#uml-behave'>Diagrammes comportementaux</a>
+    {isActive && <div>
+    <a href='#uml-cases'>Cas d’utilisation</a> 
+    <a href='#uml-activity'>Activité</a> 
+    <a href='#uml-state'>État</a> 
+    <a href='#uml-sequence'>Séquence</a> 
+    <a href='#uml-comm'>Communication</a> 
+    <a href='#uml-interactions'>Présentation des interactions</a> 
+    <a href='#uml-time'>Diagramme de temps</a> 
+    </div>}
+  </div>
+  )
+}
+
+function Accordion3() {
+  const [isActive, setIsActive] = useState(false);
+  return(
+  <div>
+    <button onClick={() => setIsActive(!isActive)} class="collapsible">{isActive ? 'v' : '>'}</button><a href='#mer-concept'>Niveau conceptuel</a>
+    {isActive && <div>
+    <a href='#mer-mcd'>Modèle Conceptuel des Données (MCD)</a> 
+    <a href='#mer-mct'>Modèle Conceptuel de Traitements (MCT)</a> 
+    </div>}
+  </div>
+  )
+}
+
+function Accordion4() {
+  const [isActive, setIsActive] = useState(false);
+  return(
+  <div>
+    <button onClick={() => setIsActive(!isActive)} class="collapsible">{isActive ? 'v' : '>'}</button><a href='#mer-orga'>Niveau organisationnel</a>
+    {isActive && <div>
+    <a href='#mer-mld'>Modèle Organisationnel des Données (MLD)</a> 
+    <a href='#mer-mlt'>Modèle Organisationnel de Traitements (MLT)</a> 
+    </div>}
+  </div>
+  )
+}
+
+function Accordion5() {
+  const [isActive, setIsActive] = useState(false);
+  return(
+  <div>
+    <button onClick={() => setIsActive(!isActive)} class="collapsible">{isActive ? 'v' : '>'}</button><a href='#mer-logic'>Niveau logique</a>
+    {isActive && <div>
+    <a href='#mer-mld'>Modèle Logique des Données (MLD)</a> 
+    <a href='#mer-mlt'>Modèle Logique de Traitements (MLT)</a> 
+    </div>}
+  </div>
+  )
+}
+
+function Accordion6() {
+  const [isActive, setIsActive] = useState(false);
+  return(
+  <div>
+    <button onClick={() => setIsActive(!isActive)} class="collapsible">{isActive ? 'v' : '>'}</button><a href='#mer-phys'>Niveau physique</a>
+    {isActive && <div>
+    <a href='#mer-mld'>Modèle Physique des Données (MPD)</a> 
+    <a href='#mer-mlt'>Modèle Physique de Traitements (MPT)</a> 
+    </div>}
+  </div>
+  )
+}
+
+function Accordion7() {
+  const [isActive, setIsActive] = useState(false);
+  return(
+  <div>
+    <button onClick={() => setIsActive(!isActive)} class="collapsible">{isActive ? 'v' : '>'}</button><a href='#mer-other'>Autres modèles</a>
+    {isActive && <div>
+    <a href='#mer-mf'>Modèle des Flux (MF)</a> 
+    <a href='#mer-me'>Modèle des États (ME)</a> 
+    <a href='#mer-ms'>Modèle de Sécurité (MS)</a> 
+    </div>}
+  </div>
+  )
+}
+
 function Sidebar() {
   return(
   <div className="App-sidebar">
-    <button type="button" class="collapsible">&#62;</button><a>Diagrammes UML</a> 
-    <button type="button" class="collapsible">&#62;</button><a>Diagrammes de structure</a> 
-    <a>Classe</a> 
-    <a>Composants</a> 
-    <a>Déploiement</a> 
-    <a>Objets</a> 
-    <a>Paquetage</a> 
-    <a>Profil</a> 
-    <a>Structure composite</a> 
-    <button type="button" class="collapsible">&#62;</button><a>Diagrammes comportementaux</a> 
-    <a>Cas d’utilisation</a> 
-    <a>Activité</a> 
-    <a>État</a> 
-    <a>Séquence</a> 
-    <a>Communication</a> 
-    <a>Présentation des interactions</a> 
-    <a>Diagramme de temps</a> 
-    <button type="button" class="collapsible">&#62;</button><a>Diagrammes MERISE</a> 
-    <button type="button" class="collapsible">&#62;</button><a>Niveau conceptuel</a> 
-    <a>Modèle Conceptuel des Données (MCD)</a> 
-    <a>Modèle Conceptuel de Traitements (MCT)</a> 
-    <button type="button" class="collapsible">&#62;</button><a>Niveau organisationnel</a> 
-    <a>Modèle Organisationnel des Données (MLD)</a> 
-    <a>Modèle Organisationnel de Traitements (MLT)</a> 
-    <button type="button" class="collapsible">&#62;</button><a>Niveau logique</a> 
-    <a>Modèle Logique des Données (MLD)</a> 
-    <a>Modèle Logique de Traitements (MLT)</a> 
-    <button type="button" class="collapsible">&#62;</button><a>Niveau physique</a> 
-    <a>Modèle Physique des Données (MPD)</a> 
-    <a>Modèle Physique de Traitements (MPT)</a> 
-    <button type="button" class="collapsible">&#62;</button><a>Autres modèles</a> 
-    <a>Modèle des Flux (MF)</a> 
-    <a>Modèle des États (ME)</a> 
-    <a>Modèle de Sécurité (MS)</a> 
+    <a>Diagrammes UML</a>
+    <Accordion1 />
+    <Accordion2 />
+    
+    <a>Diagrammes MERISE</a>
+    <Accordion3 />
+    <Accordion4 />
+    <Accordion5 />
+    <Accordion6 />
+    <Accordion7 />
   </div>
 
   )
@@ -46,56 +126,56 @@ function AppContent() {
       <h1>Catalogue des Diagrammes</h1>
       <h2>Diagrammes UML</h2>
       <ul>
-        <li className="Section">Diagrammes de structure</li>
+        <li id ='uml-structure' className="Section">Diagrammes de structure</li>
         <ol>
-          <li>Classe</li>
+          <li id='uml-class'>Classe</li>
           <p>Ce type d’exemples de diagrammes UML est souvent appelé le fondement principal de toute solution orientée objet. Son utilisation est pour la modélisation conceptuelle de la structure réelle du système et la modélisation détaillée. Il est également possible avec les diagrammes de classes de faire de la modélisation des données.
 Dans ce diagramme, les classes sont divisées en trois compartiments contenus dans une boîte. Le nom doit être en haut, suivi des attributs au milieu et des opérations en bas.</p>
-          <li>Composants</li>
-          <li>Déploiement</li>
-          <li>Objets</li>
-          <li>Paquetage</li>
-          <li>Profil</li>
-          <li>Structure composite</li>
+          <li id='uml-deploy'>Composants</li>
+          <li id='uml-objects'>Déploiement</li>
+          <li id='uml-objects'>Objets</li>
+          <li id='uml-package'>Paquetage</li>
+          <li id='uml-profile'>Profil</li>
+          <li id='uml-composite'>Structure composite</li>
         </ol>
-        <li className="Section">Diagrammes comportementaux</li>
+        <li id ='uml-behave' className="Section">Diagrammes comportementaux</li>
         <ol>
-          <li>Cas d’utilisation</li>
-          <li>Activité</li>
-          <li>État</li>
-          <li>Séquence</li>
-          <li>Communication</li>
-          <li>Présentation des interactions</li>
-          <li>Diagramme de temps</li>
+          <li id='uml-cases'>Cas d’utilisation</li>
+          <li id='uml-activity'>Activité</li>
+          <li id='uml-state'>État</li>
+          <li id='uml-sequence'>Séquence</li>
+          <li id='uml-comm'>Communication</li>
+          <li id='uml-interactions'>Présentation des interactions</li>
+          <li id='uml-time'>Diagramme de temps</li>
         </ol>
       </ul>
       <h2>Diagrammes MERISE</h2>
       <ul>
-        <li className="Section">Niveau conceptuel</li>
+        <li id='mer-concept' className="Section">Niveau conceptuel</li>
         <ol>
-          <li>Modèle Conceptuel des Données (MCD)</li>
-          <li>Modèle Conceptuel de Traitements (MCT)</li>
+          <li id='mer-mcd'>Modèle Conceptuel des Données (MCD)</li>
+          <li id='mer-mct'>Modèle Conceptuel de Traitements (MCT)</li>
         </ol>
-        <li className="Section">Niveau organisationnel</li>
+        <li id='mer-orga' className="Section">Niveau organisationnel</li>
         <ol>
-          <li>Modèle Organisationnel des Données (MLD)</li>
-          <li>Modèle Organisationnel de Traitements (MLT)</li>
+          <li id='mer-mld' >Modèle Organisationnel des Données (MLD)</li>
+          <li id='mer-mlt'>Modèle Organisationnel de Traitements (MLT)</li>
         </ol>
-        <li className="Section">Niveau logique</li>
+        <li id='mer-logic' className="Section">Niveau logique</li>
         <ol>
-          <li>Modèle Logique des Données (MLD)</li>
-          <li>Modèle Logique de Traitements (MLT)</li>
+          <li id='mer-mld'>Modèle Logique des Données (MLD)</li>
+          <li id='mer-mlt'>Modèle Logique de Traitements (MLT)</li>
         </ol>
-        <li className="Section">Niveau physique</li>
+        <li id='mer-phys' className="Section">Niveau physique</li>
         <ol>
-          <li>Modèle Physique des Données (MPD)</li>
-          <li>Modèle Physique de Traitements (MPT)</li>
+          <li id='mer-mpd'>Modèle Physique des Données (MPD)</li>
+          <li id='mer-mpt'>Modèle Physique de Traitements (MPT)</li>
         </ol>
-        <li className="Section">Autres modèles</li>
+        <li id='mer-other' className="Section">Autres modèles</li>
         <ol>
-          <li>Modèle des Flux (MF)</li>
-          <li>Modèle des États (ME)</li>
-          <li>Modèle de Sécurité (MS)</li>
+          <li id='mer-mf'>Modèle des Flux (MF)</li>
+          <li id='mer-me'>Modèle des États (ME)</li>
+          <li id='mer-ms'>Modèle de Sécurité (MS)</li>
         </ol>
       </ul>
     </div>
