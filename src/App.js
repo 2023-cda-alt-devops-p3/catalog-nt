175a73dbd56bc6c626cc5ddf658c9d2c97ee53a7
ColasCitron <ffnico@msn.com>
2023-09-20 09:26:18 +0200

    refactor: :recycle: cleaning code and refactoring for reactjs

--- src/App.js
+++ src/App.js
@@ -1,104 +1,113 @@
+function Sidebar() {
+  return(
+  <div className="App-sidebar">
+    <button type="button" class="collapsible">&#62;</button><a>Diagrammes UML</a> 
+    <button type="button" class="collapsible">&#62;</button><a>Diagrammes de structure</a> 
+    <a>Classe</a> 
+    <a>Composants</a> 
+    <a>Déploiement</a> 
+    <a>Objets</a> 
+    <a>Paquetage</a> 
+    <a>Profil</a> 
+    <a>Structure composite</a> 
+    <button type="button" class="collapsible">&#62;</button><a>Diagrammes comportementaux</a> 
+    <a>Cas d’utilisation</a> 
+    <a>Activité</a> 
+    <a>État</a> 
+    <a>Séquence</a> 
+    <a>Communication</a> 
+    <a>Présentation des interactions</a> 
+    <a>Diagramme de temps</a> 
+    <button type="button" class="collapsible">&#62;</button><a>Diagrammes MERISE</a> 
+    <button type="button" class="collapsible">&#62;</button><a>Niveau conceptuel</a> 
+    <a>Modèle Conceptuel des Données (MCD)</a> 
+    <a>Modèle Conceptuel de Traitements (MCT)</a> 
+    <button type="button" class="collapsible">&#62;</button><a>Niveau organisationnel</a> 
+    <a>Modèle Organisationnel des Données (MLD)</a> 
+    <a>Modèle Organisationnel de Traitements (MLT)</a> 
+    <button type="button" class="collapsible">&#62;</button><a>Niveau logique</a> 
+    <a>Modèle Logique des Données (MLD)</a> 
+    <a>Modèle Logique de Traitements (MLT)</a> 
+    <button type="button" class="collapsible">&#62;</button><a>Niveau physique</a> 
+    <a>Modèle Physique des Données (MPD)</a> 
+    <a>Modèle Physique de Traitements (MPT)</a> 
+    <button type="button" class="collapsible">&#62;</button><a>Autres modèles</a> 
+    <a>Modèle des Flux (MF)</a> 
+    <a>Modèle des États (ME)</a> 
+    <a>Modèle de Sécurité (MS)</a> 
+  </div>
+
+  )
+}
+
+function AppContent() {
+  return (
+    <div className="App-body">
+      <h1>Catalogue des Diagrammes</h1>
+      <h2>Diagrammes UML</h2>
+      <ul>
+        <li className="Section">Diagrammes de structure</li>
+        <ol>
+          <li>Classe</li>
+          <p>Ce type d’exemples de diagrammes UML est souvent appelé le fondement principal de toute solution orientée objet. Son utilisation est pour la modélisation conceptuelle de la structure réelle du système et la modélisation détaillée. Il est également possible avec les diagrammes de classes de faire de la modélisation des données.
+Dans ce diagramme, les classes sont divisées en trois compartiments contenus dans une boîte. Le nom doit être en haut, suivi des attributs au milieu et des opérations en bas.</p>
+          <li>Composants</li>
+          <li>Déploiement</li>
+          <li>Objets</li>
+          <li>Paquetage</li>
+          <li>Profil</li>
+          <li>Structure composite</li>
+        </ol>
+        <li className="Section">Diagrammes comportementaux</li>
+        <ol>
+          <li>Cas d’utilisation</li>
+          <li>Activité</li>
+          <li>État</li>
+          <li>Séquence</li>
+          <li>Communication</li>
+          <li>Présentation des interactions</li>
+          <li>Diagramme de temps</li>
+        </ol>
+      </ul>
+      <h2>Diagrammes MERISE</h2>
+      <ul>
+        <li className="Section">Niveau conceptuel</li>
+        <ol>
+          <li>Modèle Conceptuel des Données (MCD)</li>
+          <li>Modèle Conceptuel de Traitements (MCT)</li>
+        </ol>
+        <li className="Section">Niveau organisationnel</li>
+        <ol>
+          <li>Modèle Organisationnel des Données (MLD)</li>
+          <li>Modèle Organisationnel de Traitements (MLT)</li>
+        </ol>
+        <li className="Section">Niveau logique</li>
+        <ol>
+          <li>Modèle Logique des Données (MLD)</li>
+          <li>Modèle Logique de Traitements (MLT)</li>
+        </ol>
+        <li className="Section">Niveau physique</li>
+        <ol>
+          <li>Modèle Physique des Données (MPD)</li>
+          <li>Modèle Physique de Traitements (MPT)</li>
+        </ol>
+        <li className="Section">Autres modèles</li>
+        <ol>
+          <li>Modèle des Flux (MF)</li>
+          <li>Modèle des États (ME)</li>
+          <li>Modèle de Sécurité (MS)</li>
+        </ol>
+      </ul>
+    </div>
+  )
+}
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Catalogue des Diagrammes</h1>
-      </header>
-
-      <body>
-        <div className="App-sidebar">
-        <button type="button" class="collapsible">&#62;</button><a>Diagrammes UML</a> 
-        <button type="button" class="collapsible">&#62;</button><a>Diagrammes de structure</a> 
-          <a>Classe</a> 
-          <a>Composants</a> 
-          <a>Déploiement</a> 
-          <a>Objets</a> 
-          <a>Paquetage</a> 
-          <a>Profil</a> 
-          <a>Structure composite</a> 
-          <button type="button" class="collapsible">&#62;</button><a>Diagrammes comportementaux</a> 
-          <a>Cas d’utilisation</a> 
-          <a>Activité</a> 
-          <a>État</a> 
-          <a>Séquence</a> 
-          <a>Communication</a> 
-          <a>Présentation des interactions</a> 
-          <a>Diagramme de temps</a> 
-          <button type="button" class="collapsible">&#62;</button><a>Diagrammes MERISE</a> 
-          <button type="button" class="collapsible">&#62;</button><a>Niveau conceptuel</a> 
-          <a>Modèle Conceptuel des Données (MCD)</a> 
-          <a>Modèle Conceptuel de Traitements (MCT)</a> 
-          <button type="button" class="collapsible">&#62;</button><a>Niveau organisationnel</a> 
-          <a>Modèle Organisationnel des Données (MLD)</a> 
-          <a>Modèle Organisationnel de Traitements (MLT)</a> 
-          <button type="button" class="collapsible">&#62;</button><a>Niveau logique</a> 
-          <a>Modèle Logique des Données (MLD)</a> 
-          <a>Modèle Logique de Traitements (MLT)</a> 
-          <button type="button" class="collapsible">&#62;</button><a>Niveau physique</a> 
-          <a>Modèle Physique des Données (MPD)</a> 
-          <a>Modèle Physique de Traitements (MPT)</a> 
-          <button type="button" class="collapsible">&#62;</button><a>Autres modèles</a> 
-          <a>Modèle des Flux (MF)</a> 
-          <a>Modèle des États (ME)</a> 
-          <a>Modèle de Sécurité (MS)</a> 
-        </div>
-  
-        <div className="App-body">
-          <h2>Diagrammes UML</h2>
-          <ul>
-            <li className="Section">Diagrammes de structure</li>
-            <ol>
-              <li>Classe</li>
-              <p>Ce type d’exemples de diagrammes UML est souvent appelé le fondement principal de toute solution orientée objet. Son utilisation est pour la modélisation conceptuelle de la structure réelle du système et la modélisation détaillée. Il est également possible avec les diagrammes de classes de faire de la modélisation des données.
-  Dans ce diagramme, les classes sont divisées en trois compartiments contenus dans une boîte. Le nom doit être en haut, suivi des attributs au milieu et des opérations en bas.</p>
-              <li>Composants</li>
-              <li>Déploiement</li>
-              <li>Objets</li>
-              <li>Paquetage</li>
-              <li>Profil</li>
-              <li>Structure composite</li>
-            </ol>
-            <li className="Section">Diagrammes comportementaux</li>
-            <ol>
-              <li>Cas d’utilisation</li>
-              <li>Activité</li>
-              <li>État</li>
-              <li>Séquence</li>
-              <li>Communication</li>
-              <li>Présentation des interactions</li>
-              <li>Diagramme de temps</li>
-            </ol>
-          </ul>
-          <h2>Diagrammes MERISE</h2>
-          <ul>
-            <li className="Section">Niveau conceptuel</li>
-            <ol>
-              <li>Modèle Conceptuel des Données (MCD)</li>
-              <li>Modèle Conceptuel de Traitements (MCT)</li>
-            </ol>
-            <li className="Section">Niveau organisationnel</li>
-            <ol>
-              <li>Modèle Organisationnel des Données (MLD)</li>
-              <li>Modèle Organisationnel de Traitements (MLT)</li>
-            </ol>
-            <li className="Section">Niveau logique</li>
-            <ol>
-              <li>Modèle Logique des Données (MLD)</li>
-              <li>Modèle Logique de Traitements (MLT)</li>
-            </ol>
-            <li className="Section">Niveau physique</li>
-            <ol>
-              <li>Modèle Physique des Données (MPD)</li>
-              <li>Modèle Physique de Traitements (MPT)</li>
-            </ol>
-            <li className="Section">Autres modèles</li>
-            <ol>
-              <li>Modèle des Flux (MF)</li>
-              <li>Modèle des États (ME)</li>
-              <li>Modèle de Sécurité (MS)</li>
-            </ol>
-          </ul>
-        </div>
+      <body>       
+        <Sidebar />
+        <AppContent />
       </body>
     </div>
   );
