b9aa9e978f5cb49256cf34d3a7a2205723b0f479
ColasCitron <ffnico@msn.com>
2023-09-21 15:31:13 +0200

    feat: :sparkles: added a card builder function for main content / added content data to new data.js file

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import {dataUMLS,dataUMLC,dataMERC,dataMERO,dataMERL,dataMERP,dataMERA} from './data.js';
 
 function Accordion1() {
   const [isActive, setIsActive] = useState(false);
@@ -7,13 +8,10 @@ function Accordion1() {
     <div className="accordion">
       <button onClick={() => setIsActive(!isActive)} className="collapsible">{isActive ? '▽' : '▷'}</button><a className='sideMenu' href='#uml-structure'>Diagrammes de structure</a></div>
       {isActive && <div className='collapsible-content'>
-        <a href='#uml-class'>Classe</a> 
-        <a href='#uml-comp'>Composants</a> 
-        <a href='#uml-deploy'>Déploiement</a> 
-        <a href='#uml-objects'>Objets</a> 
-        <a href='#uml-package'>Paquetage</a> 
-        <a href='#uml-profile'>Profil</a> 
-        <a href='#uml-composite'>Structure composite</a>
+      {dataUMLS.map(dcard => (<AccContent
+              dname={dcard.dname}
+              dtag={dcard.dtag}
+          ></AccContent>))}
       </div>}
   </div>
   )
@@ -26,13 +24,10 @@ function Accordion2() {
     <div className="accordion">
       <button onClick={() => setIsActive(!isActive)} className="collapsible">{isActive ? '▽' : '▷'}</button><a className='sideMenu'  href='#uml-behave'>Diagrammes comportementaux</a></div>
       {isActive && <div className='collapsible-content'>
-      <a href='#uml-cases'>Cas d’utilisation</a> 
-      <a href='#uml-activity'>Activité</a> 
-      <a href='#uml-state'>État</a> 
-      <a href='#uml-sequence'>Séquence</a> 
-      <a href='#uml-comm'>Communication</a> 
-      <a href='#uml-interactions'>Présentation des interactions</a> 
-      <a href='#uml-time'>Diagramme de temps</a> 
+      {dataUMLC.map(dcard => (<AccContent
+              dname={dcard.dname}
+              dtag={dcard.dtag}
+          ></AccContent>))}
     </div>}
   </div>
   )
@@ -45,8 +40,10 @@ function Accordion3() {
     <div className="accordion">
       <button onClick={() => setIsActive(!isActive)} className="collapsible">{isActive ? '▽' : '▷'}</button><a className='sideMenu'  href='#mer-concept'>Niveau conceptuel</a></div>
       {isActive && <div className='collapsible-content'>
-      <a href='#mer-mcd'>Modèle Conceptuel des Données (MCD)</a> 
-      <a href='#mer-mct'>Modèle Conceptuel de Traitements (MCT)</a> 
+      {dataMERC.map(dcard => (<AccContent
+              dname={dcard.dname}
+              dtag={dcard.dtag}
+          ></AccContent>))}
     </div>}
   </div>
   )
@@ -59,8 +56,10 @@ function Accordion4() {
   <div className="accordion">
     <button onClick={() => setIsActive(!isActive)} className="collapsible">{isActive ? '▽' : '▷'}</button><a className='sideMenu' href='#mer-orga'>Niveau organisationnel</a></div>
     {isActive && <div className='collapsible-content'>
-    <a href='#mer-mld'>Modèle Organisationnel des Données (MLD)</a> 
-    <a href='#mer-mlt'>Modèle Organisationnel de Traitements (MLT)</a> 
+    {dataMERO.map(dcard => (<AccContent
+              dname={dcard.dname}
+              dtag={dcard.dtag}
+          ></AccContent>))}
     </div>}
   </div>
   )
@@ -73,8 +72,10 @@ function Accordion5() {
     <div className="accordion">
       <button onClick={() => setIsActive(!isActive)} className="collapsible">{isActive ? '▽' : '▷'}</button><a className='sideMenu' href='#mer-logic'>Niveau logique</a></div>
       {isActive && <div className='collapsible-content'>
-      <a href='#mer-mld'>Modèle Logique des Données (MLD)</a> 
-      <a href='#mer-mlt'>Modèle Logique de Traitements (MLT)</a> 
+      {dataMERL.map(dcard => (<AccContent
+              dname={dcard.dname}
+              dtag={dcard.dtag}
+          ></AccContent>))}
     </div>}
   </div>
   )
@@ -87,8 +88,10 @@ function Accordion6() {
     <div className="accordion">
       <button onClick={() => setIsActive(!isActive)} className="collapsible">{isActive ? '▽' : '▷'}</button><a className='sideMenu'  href='#mer-phys'>Niveau physique</a></div>
       {isActive && <div className='collapsible-content'>
-      <a href='#mer-mld'>Modèle Physique des Données (MPD)</a> 
-      <a href='#mer-mlt'>Modèle Physique de Traitements (MPT)</a> 
+      {dataMERP.map(dcard => (<AccContent
+              dname={dcard.dname}
+              dtag={dcard.dtag}
+          ></AccContent>))}
     </div>}
   </div>
   )
@@ -101,12 +104,17 @@ function Accordion7() {
       <div className="accordion">
         <button onClick={() => setIsActive(!isActive)} className="collapsible">{isActive ? '▽' : '▷'}</button><a className='sideMenu'  href='#mer-other'>Autres modèles</a></div>
         {isActive && <div className='collapsible-content'>
-        <a href='#mer-mf'>Modèle des Flux (MF)</a> 
-        <a href='#mer-me'>Modèle des États (ME)</a> 
-        <a href='#mer-ms'>Modèle de Sécurité (MS)</a> 
+        {dataMERA.map(dcard => (<AccContent
+              dname={dcard.dname}
+              dtag={dcard.dtag}
+          ></AccContent>))}
       </div>}
     </div>
   )
+}
+
+function AccContent({dname,dtag}) {
+  return(<a href={`#${dtag}`}>{dname}</a>)
 }
 
 function Sidebar() {
@@ -166,6 +174,21 @@ function Navbar() {
   return width < breakpoint ? <Topbar /> : <Sidebar />;
 }
 
+function DiagramCard({dname,dtag,ddesc,ddiag}) {
+  return(
+    <div>
+      <li id={dtag}>{dname}</li>
+      <div className='row'>
+        <div className='column'>
+          <p>{ddesc}</p>
+        </div> 
+        <div className='column'>
+          <img src={ddiag} alt={dname}></img>
+      </div></div>
+    </div>
+  )
+}
+
 function AppContent() {
   return (
     <div className="App-body">
@@ -174,102 +197,69 @@ function AppContent() {
       <ul>
         <li id ='uml-structure' className="Section">Diagrammes de structure</li>
         <ol>
-          <li id='uml-class'>Classe</li>
-          <div className='row'>
-          <div className='column'>
-          <p>Ce type d’exemples de diagrammes UML est souvent appelé le fondement principal de toute solution orientée objet. Son utilisation est pour la modélisation conceptuelle de la structure réelle du système et la modélisation détaillée. Il est également possible avec les diagrammes de classes de faire de la modélisation des données.
-Dans ce diagramme, les classes sont divisées en trois compartiments contenus dans une boîte. Le nom doit être en haut, suivi des attributs au milieu et des opérations en bas.</p>
-          </div> 
-          <div className='column'>
-          <img src='https://webusupload.apowersoft.info/gitmind/wp-content/uploads/2021/05/class-diagram.jpg.webp' alt='class diagram example'></img>
-          </div></div>
-          <li id='uml-deploy'>Composants</li>
-          <div className='row'>
-          <div className='column'>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-          </div> 
-          <div className='column'>
-          <img src='https://webusupload.apowersoft.info/gitmind/wp-content/uploads/2021/05/class-diagram.jpg.webp' alt='class diagram example'></img>
-          </div></div>
-          <li id='uml-objects'>Déploiement</li>
-          <div className='row'>
-          <div className='column'>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-          </div> 
-          <div className='column'>
-          <img src='https://webusupload.apowersoft.info/gitmind/wp-content/uploads/2021/05/class-diagram.jpg.webp' alt='class diagram example'></img>
-          </div></div>
-          <li id='uml-objects'>Objets</li>
-          <div className='row'>
-          <div className='column'>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-          </div> 
-          <div className='column'>
-          <img src='https://webusupload.apowersoft.info/gitmind/wp-content/uploads/2021/05/class-diagram.jpg.webp' alt='class diagram example'></img>
-          </div></div>
-          <li id='uml-package'>Paquetage</li>
-          <div className='row'>
-          <div className='column'>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-          </div> 
-          <div className='column'>
-          <img src='https://webusupload.apowersoft.info/gitmind/wp-content/uploads/2021/05/class-diagram.jpg.webp' alt='class diagram example'></img>
-          </div></div>
-          <li id='uml-profile'>Profil</li>
-          <div className='row'>
-          <div className='column'>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-          </div> 
-          <div className='column'>
-          <img src='https://webusupload.apowersoft.info/gitmind/wp-content/uploads/2021/05/class-diagram.jpg.webp' alt='class diagram example'></img>
-          </div></div>
-          <li id='uml-composite'>Structure composite</li>
-          <div className='row'>
-          <div className='column'>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-          </div> 
-          <div className='column'>
-          <img src='https://webusupload.apowersoft.info/gitmind/wp-content/uploads/2021/05/class-diagram.jpg.webp' alt='class diagram example'></img>
-          </div></div>
+          {dataUMLS.map(dcard => (<DiagramCard
+              dname={dcard.dname}
+              dtag={dcard.dtag}
+              ddesc={dcard.ddesc}
+              ddiag={dcard.ddiag}
+          ></DiagramCard>))}
         </ol>
         <li id ='uml-behave' className="Section">Diagrammes comportementaux</li>
         <ol>
-          <li id='uml-cases'>Cas d’utilisation</li>
-          <li id='uml-activity'>Activité</li>
-          <li id='uml-state'>État</li>
-          <li id='uml-sequence'>Séquence</li>
-          <li id='uml-comm'>Communication</li>
-          <li id='uml-interactions'>Présentation des interactions</li>
-          <li id='uml-time'>Diagramme de temps</li>
+        {dataUMLC.map(dcard => (<DiagramCard
+              dname={dcard.dname}
+              dtag={dcard.dtag}
+              ddesc={dcard.ddesc}
+              ddiag={dcard.ddiag}
+          ></DiagramCard>))}
         </ol>
       </ul>
       <h2 id ='mer'>Diagrammes MERISE</h2>
       <ul>
         <li id='mer-concept' className="Section">Niveau conceptuel</li>
         <ol>
-          <li id='mer-mcd'>Modèle Conceptuel des Données (MCD)</li>
-          <li id='mer-mct'>Modèle Conceptuel de Traitements (MCT)</li>
+        {dataMERC.map(dcard => (<DiagramCard
+              dname={dcard.dname}
+              dtag={dcard.dtag}
+              ddesc={dcard.ddesc}
+              ddiag={dcard.ddiag}
+          ></DiagramCard>))}
         </ol>
         <li id='mer-orga' className="Section">Niveau organisationnel</li>
         <ol>
-          <li id='mer-mld' >Modèle Organisationnel des Données (MLD)</li>
-          <li id='mer-mlt'>Modèle Organisationnel de Traitements (MLT)</li>
+        {dataMERO.map(dcard => (<DiagramCard
+              dname={dcard.dname}
+              dtag={dcard.dtag}
+              ddesc={dcard.ddesc}
+              ddiag={dcard.ddiag}
+          ></DiagramCard>))}
         </ol>
         <li id='mer-logic' className="Section">Niveau logique</li>
         <ol>
-          <li id='mer-mld'>Modèle Logique des Données (MLD)</li>
-          <li id='mer-mlt'>Modèle Logique de Traitements (MLT)</li>
+        {dataMERL.map(dcard => (<DiagramCard
+              dname={dcard.dname}
+              dtag={dcard.dtag}
+              ddesc={dcard.ddesc}
+              ddiag={dcard.ddiag}
+          ></DiagramCard>))}
         </ol>
         <li id='mer-phys' className="Section">Niveau physique</li>
         <ol>
-          <li id='mer-mpd'>Modèle Physique des Données (MPD)</li>
-          <li id='mer-mpt'>Modèle Physique de Traitements (MPT)</li>
+        {dataMERP.map(dcard => (<DiagramCard
+              dname={dcard.dname}
+              dtag={dcard.dtag}
+              ddesc={dcard.ddesc}
+              ddiag={dcard.ddiag}
+          ></DiagramCard>))}
         </ol>
         <li id='mer-other' className="Section">Autres modèles</li>
         <ol>
-          <li id='mer-mf'>Modèle des Flux (MF)</li>
-          <li id='mer-me'>Modèle des États (ME)</li>
-          <li id='mer-ms'>Modèle de Sécurité (MS)</li>
+        {dataMERA.map(dcard => (<DiagramCard
+              dname={dcard.dname}
+              dtag={dcard.dtag}
+              ddesc={dcard.ddesc}
+              ddiag={dcard.ddiag}
+          ></DiagramCard>))}
         </ol>
       </ul>
     </div>
---
+++ src/data.js
@@ -0,0 +1,170 @@
+export const dataUMLS = [
+    {
+        dname: 'Classe',
+        dtag: 'uml-class',
+        ddesc: 'Ce type d’exemples de diagrammes UML est souvent appelé le fondement principal de toute solution orientée objet. Son utilisation est pour la modélisation conceptuelle de la structure réelle du système et la modélisation détaillée. Il est également possible avec les diagrammes de classes de faire de la modélisation des données. Dans ce diagramme, les classes sont divisées en trois compartiments contenus dans une boîte. Le nom doit être en haut, suivi des attributs au milieu et des opérations en bas.',
+        ddiag: 'https://webusupload.apowersoft.info/gitmind/wp-content/uploads/2021/05/class-diagram.jpg.webp'
+    },
+    {
+        dname: 'Composants',
+        dtag: 'uml-comp',
+        ddesc: 'TBD',
+        ddiag: 'TBD'
+    },
+    {
+        dname: 'Déploiement',
+        dtag: 'uml-deploy',
+        ddesc: 'TBD',
+        ddiag: 'TBD'
+    },
+    {
+        dname: 'Objets',
+        dtag: 'uml-objects',
+        ddesc: 'TBD',
+        ddiag: 'TBD'
+    },
+    {
+        dname: 'Paquetage',
+        dtag: 'uml-package',
+        ddesc: 'TBD',
+        ddiag: 'TBD'
+    },
+    {
+        dname: 'Profil',
+        dtag: 'uml-profile',
+        ddesc: 'TBD',
+        ddiag: 'TBD'
+    },
+    {
+        dname: 'Structure composite',
+        dtag: 'uml-composite',
+        ddesc: 'TBD',
+        ddiag: 'TBD'
+    },
+  ]
+
+  export const dataUMLC = [
+    {
+        dname: 'Cas d’utilisation',
+        dtag: 'uml-cases',
+        ddesc: 'TBD',
+        ddiag: 'TBD'
+    },
+    {
+        dname: 'Activité',
+        dtag: 'uml-activity',
+        ddesc: 'TBD',
+        ddiag: 'TBD'
+    },
+    {
+        dname: 'État',
+        dtag: 'uml-state',
+        ddesc: 'TBD',
+        ddiag: 'TBD'
+    },
+    {
+        dname: 'Séquence',
+        dtag: 'uml-sequence',
+        ddesc: 'TBD',
+        ddiag: 'TBD'
+    },
+    {
+        dname: 'Communication',
+        dtag: 'uml-comm',
+        ddesc: 'TBD',
+        ddiag: 'TBD'
+    },
+    {
+        dname: 'Présentation des interactions',
+        dtag: 'uml-interactions',
+        ddesc: 'TBD',
+        ddiag: 'TBD'
+    },
+    {
+        dname: 'Diagramme de temps',
+        dtag: 'uml-time',
+        ddesc: 'TBD',
+        ddiag: 'TBD'
+    }
+  ]
+
+  export const dataMERC = [
+    {
+        dname: 'Modèle Conceptuel des Données (MCD)',
+        dtag: 'mer-mcd',
+        ddesc: 'TBD',
+        ddiag: 'TBD'
+    },
+    {
+        dname: 'Modèle Conceptuel de Traitements (MCT)',
+        dtag: 'mer-mct',
+        ddesc: 'TBD',
+        ddiag: 'TBD'
+    }
+]
+
+export const dataMERO = [
+    {
+        dname: 'Modèle Organisationnel des Données (MLD)',
+        dtag: 'mer-mld',
+        ddesc: 'TBD',
+        ddiag: 'TBD'
+    },
+    {
+        dname: 'Modèle Organisationnel de Traitements (MLT)',
+        dtag: 'mer-mlt',
+        ddesc: 'TBD',
+        ddiag: 'TBD'
+    }
+]
+
+export const dataMERL = [
+    {
+        dname: 'Modèle Logique des Données (MLD)',
+        dtag: 'mer-mld',
+        ddesc: 'TBD',
+        ddiag: 'TBD'
+    },
+    {
+        dname: 'Modèle Logique de Traitements (MLT)',
+        dtag: 'mer-mlt',
+        ddesc: 'TBD',
+        ddiag: 'TBD'
+    }
+]
+
+export const dataMERP = [
+    {
+        dname: 'Modèle Physique des Données (MPD)',
+        dtag: 'mer-mld',
+        ddesc: 'TBD',
+        ddiag: 'TBD'
+    },
+    {
+        dname: 'Modèle Physique de Traitements (MPT)',
+        dtag: 'mer-mlt',
+        ddesc: 'TBD',
+        ddiag: 'TBD'
+    }
+]
+
+export const dataMERA = [
+    {
+        dname: 'Modèle des Flux (MF)',
+        dtag: 'mer-mf',
+        ddesc: 'TBD',
+        ddiag: 'TBD'
+    },
+    {
+        dname: 'Modèle des États (ME)',
+        dtag: 'mer-me',
+        ddesc: 'TBD',
+        ddiag: 'TBD'
+    },
+    {
+        dname: 'Modèle de Sécurité (MS)',
+        dtag: 'mer-ms',
+        ddesc: 'TBD',
+        ddiag: 'TBD'
+    }
+]
